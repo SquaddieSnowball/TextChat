94d5fc70ef85a75f045a1cee178c66c9b5e50c9a
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2025-07-26 13:42:42 +0300

    feat(domain): add chat message entities

---
+++ src/TextChat.Domain/Entities/ClientChatMessage.cs
@@ -0,0 +1,3 @@
+﻿namespace TextChat.Domain.Entities;
+
+public record class ClientChatMessage(DateTime SentTimestamp, string Body);
---
+++ src/TextChat.Domain/Entities/ServerChatMessage.cs
@@ -0,0 +1,5 @@
+﻿using System.Net;
+
+namespace TextChat.Domain.Entities;
+
+public record class ServerChatMessage(IPAddress ClientIPAddress, DateTime ReceivedTimestamp, string Body);
