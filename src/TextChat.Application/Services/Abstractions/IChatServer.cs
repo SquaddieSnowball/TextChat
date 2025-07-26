74b873bd8bc602316ff02a3e99d9a6bec37ea898
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2025-07-26 23:04:58 +0300

    feat(application): add chat server abstractions

---
+++ src/TextChat.Application/Services/Abstractions/IChatServer.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using TextChat.Domain.Entities;
+using TextChat.Domain.Primitives;
+
+namespace TextChat.Application.Services.Abstractions;
+
+public interface IChatServer : IDisposable
+{
+	bool Started { get; }
+
+	IPEndPoint? ServerEndpoint { get; }
+
+	IEnumerable<IPEndPoint> ConnectedClientEndpoints { get; }
+
+	event EventHandler<IPEndPoint>? ServerStarted;
+
+	event EventHandler<IPEndPoint>? ServerStopped;
+
+	event EventHandler<IPEndPoint>? ClientConnected;
+
+	event EventHandler<IPEndPoint>? ClientDisconnected;
+
+	event EventHandler<Error>? ErrorAcceptingClient;
+
+	event EventHandler<ClientChatMessage>? MessageReceived;
+
+	event EventHandler<Error>? ErrorReceivingMessage;
+
+	event EventHandler<ServerChatMessage>? MessageBroadcasted;
+
+	event EventHandler<Error>? ErrorBroadcastingClientMessage;
+
+	Result Start(string ipAddress, int port);
+
+	void Stop();
+
+	Task<Result> BroadcastMessage(string message);
+}
---
+++ src/TextChat.Application/Services/Abstractions/IServerChatMessageBuilder.cs
@@ -0,0 +1,9 @@
+﻿using TextChat.Domain.Entities;
+using TextChat.Domain.Primitives;
+
+namespace TextChat.Application.Services.Abstractions;
+
+public interface IServerChatMessageBuilder
+{
+	Task<Result<string>> Build(ServerChatMessage message);
+}
