75689ef8f39f5fbf748ea06d47b456add53abaf3
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2025-07-26 23:05:53 +0300

    feat(domain): add server client entity

---
+++ src/TextChat.Domain/Entities/ServerClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TextChat.Domain.Entities;
+
+public record class ServerClient(
+	Guid Id,
+	TcpClient TcpClient,
+	IPEndPoint Endpoint,
+	StreamReader StreamReader,
+	StreamWriter StreamWriter);
