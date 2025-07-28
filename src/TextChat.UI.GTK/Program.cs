f9d6177a944d30b6a14049727fa6cb0f86ccfebe
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2025-07-28 22:24:59 +0300

    feat(gtkui): add chat functionality

--- src/TextChat.UI.GTK/Program.cs
+++ src/TextChat.UI.GTK/Program.cs
@@ -1,20 +1,46 @@
-using GtkApplication = Gtk.Application;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TextChat.Application.Services;
+using TextChat.Application.Services.Abstractions;
 using TextChat.UI.GTK.Views;
+using GtkApplication = Gtk.Application;
 
 namespace TextChat.UI.GTK;
 
 internal class Program
 {
+	public static IHost AppHost { get; private set; }
+
+	static Program()
+	{
+		IHostBuilder builder = Host.CreateDefaultBuilder();
+
+		builder
+			.ConfigureLogging(logging =>
+			{
+				logging
+					.ClearProviders();
+			})
+			.ConfigureServices((context, services) =>
+			{
+				services
+					.AddTransient<IChatClient, ChatClient>()
+					.AddTransient<IChatServer, ChatServer>()
+					.AddTransient<IChatMessageParser, ChatMessageParser>()
+					.AddTransient<IChatMessageBuilder, ChatMessageBuilder>()
+					.AddSingleton<MainWindow>();
+			});
+
+		AppHost = builder.Build();
+	}
+
 	[STAThread]
 	public static void Main()
 	{
 		GtkApplication.Init();
 
-		GtkApplication application = new("ru.Trackmark.TextChat", GLib.ApplicationFlags.None);
-		application.Register(GLib.Cancellable.Current);
-
-		MainWindow mainWindow = new();
-		application.AddWindow(mainWindow);
+		MainWindow mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
 		mainWindow.Show();
 
 		GtkApplication.Run();
--- src/TextChat.UI.GTK/Views/MainWindow.cs
+++ src/TextChat.UI.GTK/Views/MainWindow.cs
@@ -1,4 +1,7 @@
 using Gtk;
+using System.Net;
+using TextChat.Application.Services.Abstractions;
+using TextChat.Domain.Primitives;
 using GtkApplication = Gtk.Application;
 using UIObject = Gtk.Builder.ObjectAttribute;
 
@@ -6,27 +9,197 @@ namespace TextChat.UI.GTK.Views;
 
 internal class MainWindow : Window
 {
-	[UIObject] private readonly Label _label = default!;
-	[UIObject] private readonly Button _button = default!;
+	private const int Port = 12345;
 
-	private int _counter;
+	private readonly IChatClient _chatClient;
+	private readonly IChatServer _chatServer;
 
-	public MainWindow() : this(new Builder("MainWindow.glade")) { }
+	[UIObject] private readonly Entry _ipAddress = default!;
+	[UIObject] private readonly CheckButton _serverMode = default!;
+	[UIObject] private readonly Button _connect = default!;
+	[UIObject] private readonly TextView _chatHistory = default!;
+	[UIObject] private readonly Entry _message = default!;
+	[UIObject] private readonly Button _send = default!;
 
-	private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+	private bool _connected;
+
+	public MainWindow(IChatClient chatClient, IChatServer chatServer)
+		: this(new Builder("MainWindow.glade"), chatClient, chatServer) { }
+
+	private MainWindow(Builder builder, IChatClient chatClient, IChatServer chatServer)
+		: base(builder.GetRawOwnedObject("MainWindow"))
 	{
 		builder.Autoconnect(this);
 
-		DeleteEvent += WindowOnDeleteEvent;
+		(_chatClient, _chatServer) = (chatClient, chatServer);
 
-		_button.Clicked += ButtonOnClicked;
+		_connect.Clicked += ConnectOnClicked;
+		_send.Clicked += SendOnClicked;
+
+		DeleteEvent += WindowOnDeleteEvent;
 	}
 
-	private void WindowOnDeleteEvent(object sender, DeleteEventArgs a) => GtkApplication.Quit();
-
-	private void ButtonOnClicked(object? sender, EventArgs a)
+	private async void ConnectOnClicked(object? sender, EventArgs e)
 	{
-		_counter++;
-		_label.Text = $"Hello World! This button has been clicked {_counter} time(s).";
+		if (!_serverMode.Active)
+			await ClientConnect();
+		else
+			ServerConnect();
+	}
+
+	private async void SendOnClicked(object? sender, EventArgs e)
+	{
+		if (!_serverMode.Active)
+			await _chatClient.SendMessage(_message.Text);
+		else
+			await _chatServer.BroadcastMessage(_message.Text);
+
+		_message.Text = string.Empty;
+	}
+
+	private async Task ClientConnect()
+	{
+		if (!_connected)
+		{
+			SubscribeToClientChatEvents();
+
+			Result connectResult = await _chatClient.Connect(_ipAddress.Text, Port);
+
+			if (connectResult.IsSuccess)
+			{
+				SwitchConnectControls(true);
+
+				_connected = true;
+			}
+			else
+				UnsubscribeFromClientChatEvents();
+		}
+		else
+		{
+			_chatClient.Disconnect();
+
+			SwitchConnectControls(false);
+
+			_connected = false;
+
+			UnsubscribeFromClientChatEvents();
+		}
+	}
+
+	private void ServerConnect()
+	{
+		if (!_connected)
+		{
+			SubscribeToServerChatEvents();
+
+			Result connectResult = _chatServer.Start(_ipAddress.Text, Port);
+
+			if (connectResult.IsSuccess)
+			{
+				SwitchConnectControls(true);
+
+				_connected = true;
+			}
+			else
+				UnsubscribeFromServerChatEvents();
+		}
+		else
+		{
+			_chatServer.Stop();
+
+			SwitchConnectControls(false);
+
+			_connected = false;
+
+			UnsubscribeFromServerChatEvents();
+		}
+	}
+
+	private void SwitchConnectControls(bool connected)
+	{
+		if (connected)
+		{
+			_ipAddress.Sensitive = false;
+			_serverMode.Sensitive = false;
+			_connect.Label = "Disconnect";
+			_message.Sensitive = true;
+			_send.Sensitive = true;
+		}
+		else
+		{
+			_ipAddress.Sensitive = true;
+			_serverMode.Sensitive = true;
+			_connect.Label = "Connect";
+			_message.Sensitive = false;
+			_send.Sensitive = false;
+		}
+	}
+
+	private void SubscribeToClientChatEvents()
+	{
+		_chatClient.ClientConnected += ChatClientOnClientConnected;
+		_chatClient.ClientDisconnected += ChatClientOnClientDisconnected;
+		_chatClient.MessageReceived += ChatOnMessage;
+	}
+
+	private void SubscribeToServerChatEvents()
+	{
+		_chatServer.ServerStarted += ChatServerOnServerStarted;
+		_chatServer.ServerStopped += ChatServerOnServerStopped;
+		_chatServer.ClientConnected += ChatServerOnClientConnected;
+		_chatServer.ClientDisconnected += ChatServerOnClientDisconnected;
+		_chatServer.MessageBroadcasted += ChatOnMessage;
+	}
+
+	private void UnsubscribeFromClientChatEvents()
+	{
+		_chatClient.ClientConnected -= ChatClientOnClientConnected;
+		_chatClient.ClientDisconnected -= ChatClientOnClientDisconnected;
+		_chatClient.MessageReceived -= ChatOnMessage;
+	}
+
+	private void UnsubscribeFromServerChatEvents()
+	{
+		_chatServer.ServerStarted -= ChatServerOnServerStarted;
+		_chatServer.ServerStopped -= ChatServerOnServerStopped;
+		_chatServer.ClientConnected -= ChatServerOnClientConnected;
+		_chatServer.ClientDisconnected -= ChatServerOnClientDisconnected;
+		_chatServer.MessageBroadcasted -= ChatOnMessage;
+	}
+
+	private void ChatOnMessage(object? sender, Domain.Entities.ServerChatMessage e) =>
+		_chatHistory.Buffer.Text += $"{e.ClientIPAddress}: {e.Body}{Environment.NewLine}";
+
+	private void ChatClientOnClientConnected(object? sender, IPEndPoint e) =>
+		_chatHistory.Buffer.Text += $"You have joined the chat{Environment.NewLine}";
+
+	private void ChatClientOnClientDisconnected(object? sender, IPEndPoint e)
+	{
+		_chatHistory.Buffer.Text += $"You have left the chat{Environment.NewLine}";
+
+		SwitchConnectControls(false);
+
+		_connected = false;
+
+		UnsubscribeFromClientChatEvents();
+	}
+
+	private void ChatServerOnServerStarted(object? sender, IPEndPoint e) =>
+		_chatHistory.Buffer.Text += $"Server started at {e.Address}:{e.Port}{Environment.NewLine}";
+
+	private void ChatServerOnServerStopped(object? sender, IPEndPoint e) =>
+		_chatHistory.Buffer.Text += $"Server stopped at {e.Address}:{e.Port}{Environment.NewLine}";
+
+	private void ChatServerOnClientConnected(object? sender, IPEndPoint e) =>
+		_chatHistory.Buffer.Text += $"{e.Address} joined{Environment.NewLine}";
+
+	private void ChatServerOnClientDisconnected(object? sender, IPEndPoint e) =>
+		_chatHistory.Buffer.Text += $"{e.Address} left{Environment.NewLine}";
+
+	private void WindowOnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Program.AppHost.Dispose();
+
+		GtkApplication.Quit();
 	}
 }
